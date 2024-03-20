46b17240c6f6e21f0d9f08348b56e5e8b3cfda15
Nostraw <Nostraw1@yandex.ru>
2024-03-20 23:47:13 +0300

    начало начал

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bazarvito_secret_key'
 
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html', title='Bazarvito')
 
