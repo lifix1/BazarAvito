37823b8a5299312bfa3c983b7bb244e971322889
Nostraw <Nostraw1@yandex.ru>
2024-03-20 23:46:07 +0300

    начало начал

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ app.config['SECRET_KEY'] = 'bazarvito_secret_key'
 @app.route('/index')
 def index():
     return render_template('index.html', title='Bazarvito')
+
+
+if __name__ == '__main__':
+    # create_db()
+    app.run(host='127.0.0.1', port=8080)
