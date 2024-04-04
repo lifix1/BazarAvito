73f0ff8d35f7d44cc62d5a49b917166dc36acae1
Nostraw <Nostraw1@yandex.ru>
2024-04-04 17:27:38 +0300

    чё то вышло

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, redirect
-from flask_login import LoginManager, login_user
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 from data import db_session
 from data.users import User
 from forms.RegistrForm import RegisterForm
 from forms.LoginForm import LoginForm
+import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bazarvito_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
 db_session.global_init("db/users.db")
+
+
+def connectdb(name):
+    con = sqlite3.connect(name)
+    cur = con.cursor()
 
 
 @login_manager.user_loader
@@ -20,7 +26,8 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Bazarvito')
+    return render_template('index.html', title='Bazarvito',
+                           current_user=current_user if current_user.is_authenticated else False)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -55,17 +62,32 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return redirect("/")
+            return redirect(request.args.get("next") or url_for("profile"))
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
